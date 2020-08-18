94549fb2c10b05b047acb1f2f9e226f6c6fa359e
dylanz <zzh9098-=>
2020-08-18 14:44:25 +0800

    Update ConfigUtil.java

--- src/main/java/com/github/dylanz666/util/senior/ConfigUtil.java
+++ src/main/java/com/github/dylanz666/util/senior/ConfigUtil.java
@@ -43,16 +43,4 @@ public class ConfigUtil {
             return null;
         }
     }
-
-    public class Constant {
-        public final String getDbConnectionUrl = "https://10.9.77.61:14444/api/connections";
-        public final String queryDbUrl = "https://10.9.77.61:14444/api/query-result";
-        public final String getSecurityCodeUrl = "http://sjtsmozilla01:8084/api/getSecurityCode";
-        public final String awsIvrOrigin = "http://10.9.76.178:8080/ivr-aws-lambda-connect";
-        public final int boseOverrideCode = "pr".equals(getEnv()) ? 953157 : 24571;
-        public final int ehiOrganizationId = 45;
-        public final int boseDefaultZipCode = 90001;
-        public final String boseDefaultAlliancePhone = "8882960117";
-        public final String boseDefaultInboundNumber = "888-296-0117";
-    }
 }
