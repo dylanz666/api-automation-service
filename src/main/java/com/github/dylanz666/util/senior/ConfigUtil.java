e33793be4f8df89b9161bc0b24ed18be6bba9e00
dylanz <zzh9098-=>
2020-08-18 14:58:52 +0800

    Update ConfigUtil.java

--- src/main/java/com/github/dylanz666/util/senior/ConfigUtil.java
+++ src/main/java/com/github/dylanz666/util/senior/ConfigUtil.java
@@ -10,10 +10,10 @@ import org.springframework.beans.factory.annotation.Autowired;
  * @since : 08/05/2020
  **/
 public class ConfigUtil {
-    private static String env = "";
-
     @Autowired
     private FileUtil fileUtil;
+
+    private String env;
 
     public void setEnv() {
         env = (null != System.getProperty("env")) ? System.getProperty("env").toLowerCase() : "cm";
